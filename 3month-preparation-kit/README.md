9f35aba2352c4a60557bd55f2a5452502df9e268
ES Choi <eschoi@ESui-MacBookAir.local>
2022-04-20 23:20:53 +0900

    week5-1

--- 3month-preparation-kit/README.md
+++ 3month-preparation-kit/README.md
@@ -1,7 +1,7 @@
 # 해커랭크 정리 
 ## week1-1 PlusMinus
 ```js
-filter(값);
+배열.filter(함수(인자));
 
 숫자.tofixed();
 ```
---
+++ 3month-preparation-kit/week5-1.js
@@ -0,0 +1,34 @@
+console.clear();
+
+// Example
+
+// Pick any two elements, say .
+
+// Testing for all pairs, the solution  provides the minimum unfairness.
+
+// Note: Integers in  may not be unique.
+
+// Function Description
+
+// Complete the maxMin function in the editor below.
+// maxMin has the following parameter(s):
+
+// int k: the number of elements to select
+// int arr[n]:: an array of integers
+
+
+const k = 3
+const arr = [10,  100, 300, 200, 1000, 20, 30]
+
+function maxMin(k, arr) {
+let res = Infinity;
+arr.sort((a,b) => (a-b));
+for (let i = 0; i <= arr.length-k ; i++ ) {
+    if (arr[i+k-1] - arr[i] < res) {
+        res = arr[i+k-1] - arr[i]
+    } 
+}
+return res;
+}
+
+maxMin(k,arr)
